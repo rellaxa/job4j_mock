084083a46d9f4a0489851ba8d41c26b4d0cfb825
relaxa <ewg.nadeev@yandex.ru>
2025-07-11 18:48:41 +0300

    1. О проекте CheckDev [#505052 #427927]

---
+++ services/notification/src/main/java/ru/checkdev/notification/domain/RoleDTO.java
@@ -0,0 +1,15 @@
+package ru.checkdev.notification.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 05.10.2023
+ */
+@Data
+@AllArgsConstructor
+public class RoleDTO {
+
+    private int id;
+}
