1f479f8f08a46ff805cd07538fd56439e2d28ddd
relaxa <ewg.nadeev@yandex.ru>
2025-07-13 22:40:31 +0300

    4. Микросервисные системы [#338434 #428370]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,10 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,6 +20,7 @@ public class IndexController {
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -41,7 +39,9 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
+        var interviews = interviewsService.getByType(1);
+        model.addAttribute("new_interviews", interviews);
+        model.addAttribute("profiles", profilesService.getByInterviews(interviews));
         return "index";
     }
 }
